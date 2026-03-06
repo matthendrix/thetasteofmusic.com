8fd4a77c6a573cfb71a44d427040c265de3a51b1
Matt Emery <matthendrix@gmail.com>
2026-03-06 22:21:37 +1100

    Fix John Lydon video ID

--- src/data/artists.ts
+++ src/data/artists.ts
@@ -33,7 +33,7 @@ export const artists: Artist[] = [
   { slug: "george-harrison", artist: "George Harrison", song: "Something", embed: "UelDrZ1aFeY" },
   { slug: "roger-daltry", artist: "Roger Daltry", song: "See Me Feel Me", embed: "NzuNJod_o7g" },
   { slug: "nico", artist: "Nico", song: "Femme Fatale", embed: "ggHPtzVSEeE" },
-  { slug: "john-lydon", artist: "John Lydon", song: "Pretty Vacant", embed: "VcauCclfytI" },
+  { slug: "john-lydon", artist: "John Lydon", song: "Pretty Vacant", embed: "R6GDdKrQ8EI" },
   { slug: "nina-simone", artist: "Nina Simone", song: "I Shall Be Released (live)", embed: "w-du8MDE8nk" },
   { slug: "scott-walker", artist: "Scott Walker", song: "Plastic palace people", embed: "ayrS74ktTyE" },
   { slug: "neil-young", artist: "Neil Young", song: "Cortez The Killer", embed: "uX9k9aoX6gk" },
